a0f691e72b4376eb0d84661216a6433978622096
seadavdic <sead.avdic@hotmail.com>
2017-11-12 16:15:10 +0100

    add: Neue XFList App

---
+++ XFListView/XFListView/XFListView/PluralsightCourse.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+
+namespace XFListView
+{
+    public class PluralsightCourse
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+
+        public static List<PluralsightCourse> GetCourseList()
+        {
+            var courseList = new List<PluralsightCourse>();
+            courseList.Add(new PluralsightCourse
+            {
+                Title = "Building Cross Platform Apps with Xamarin Part 1",
+                Author = "Jim Wilson"
+            });
+            courseList.Add(new PluralsightCourse
+            {
+                Title = "Building Cross Platform Apps with Xamarin Part 2",
+                Author = "Jim Wilson"
+            });
+            courseList.Add(new PluralsightCourse
+            {
+                Title = "Building Google Glass Apps with c# and Xamarin",
+                Author = "Jim Wilson"
+            });
+            courseList.Add(new PluralsightCourse
+            {
+                Title = "Android for .NET Developers",
+                Author = "Jim Wilson"
+            });
+
+            return courseList;
+        }
+
+        public override string ToString()
+        {
+            return Title;
+        }
+    }
+}
